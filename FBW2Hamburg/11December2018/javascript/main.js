1e216c70787f050a25dc7f0db5235d386faa71c6
thechiefalone <alperenozkan@gmail.com>
2018-12-12 09:15:58 +0100

    update

---
+++ FBW2Hamburg/11December2018/javascript/main.js
@@ -0,0 +1,12 @@
+window.onload=function(){
+    // document.getElementById('result').innerHTML='<h1>FBW</h1>';
+
+let x = 4 ;
+let z = 5 ;
+let y = x + z ;
+let f = y == x;
+console.log(z,x,y);
+console.log(y+x+z);
+console.log(y+''+x+''+z);
+console.log(f);
+}
