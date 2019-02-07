89242db38d44d3caa705bc9ac46df222be645922
thechiefalone <alperenozkan@gmail.com>
2019-02-07 09:50:43 +0100

    update

---
+++ FBW2Hamburg/06Februar2019/main.js
@@ -0,0 +1,6 @@
+console.log("hi i am in the page1");
+const someVar = 12;
+console.log(someVar);
+
+var i = 5;
+console.log(i);
---
+++ FBW2Hamburg/06Februar2019/operations.js
@@ -0,0 +1,10 @@
+let num1 = 5;
+let num2 = Math.pow(num1,2);
+console.log(num2);
+
+{
+    let num1=3;
+    let num2=4;
+    let num3=num2/(2*num1);
+    console.log(num1--);
+}
