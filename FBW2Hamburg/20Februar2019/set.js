2210fdb327811a376d8cfb471a4104f2c4a83d85
thechiefalone <alperenozkan@gmail.com>
2019-02-20 13:23:34 +0100

    set

--- FBW2Hamburg/20Februar2019/set.js
+++ FBW2Hamburg/20Februar2019/set.js
@@ -4,4 +4,56 @@ console.log(array.has(8));
 
 array.forEach(num => {
     console.log(num);
-})
+});
+
+console.log("new line");
+
+for (let num of array) {
+    console.log(num);
+}
+
+console.log("new line");
+
+let items = array.values();
+console.log(items.next());
+console.log(items.next());
+console.log(items.next());
+console.log(items.next());
+console.log(items.next());
+console.log(items.next());
+console.log(items.next());
+console.log(items.next());
+
+console.log("new line");
+
+console.clear();
+
+// let dictionary = new Map([
+//    [key,value]
+// ]);
+let dictionary = new Map([
+    ["dog", "hund"],
+    ["table", "tisch"],
+    ["cold", "kalt"],
+    ["new", "neu"]
+]);
+
+console.log(dictionary.get("dog"));
+
+dictionary.set("animal","tiere");
+
+console.log(dictionary);
+
+console.log(dictionary.size);
+
+dictionary.set("1","999");
+
+console.log(dictionary);
+
+dictionary.delete("1");
+
+console.log(dictionary);
+
+
+
+
