942fdea284f9f85a534de31b60128e52c4a3702b
thechiefalone <alperenozkan@gmail.com>
2019-02-27 13:59:53 +0100

    op

--- FBW2Hamburg/2019Februar_27/animal.js
+++ FBW2Hamburg/2019Februar_27/animal.js
@@ -1,22 +1,22 @@
-class Animal { 
-    constructor(name) {
-      this.name = name;
-    }
-    speak() {
-        console.log(this.name + ' makes a noise.');
-      }
-    }
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+  speak() {
+    console.log(this.name + ' makes a noise.');
+  }
+}
 
-    class Lion extends Animal{
-        constructor (name,color){
-            super (name);
-            this.color=color;
-        }
-        speak(){
-            console.log(this.name+" makes a roar");
-        }
-    }
+class Lion extends Animal {
+  constructor(name, color) {
+    super(name);
+    this.color = color;
+  }
+  speak() {
+    console.log(this.name + " makes a roar");
+  }
+}
 
-    let d=new Lion("king","white");
-    d.speak();
-    console.log(d.color);
+let d = new Lion("king", "white");
+d.speak();
+console.log(d.color);
