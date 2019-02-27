446783141147208a9f349a42f7d15063af5c961f
thechiefalone <alperenozkan@gmail.com>
2019-02-27 11:56:53 +0100

    op

--- FBW2Hamburg/2019Februar_27/rectangle.js
+++ FBW2Hamburg/2019Februar_27/rectangle.js
@@ -1,13 +1,27 @@
-class rectangle{
-    constructor(height,width){
-        this.height=height;
-        this.width=width;
+class rectangle {
+    width;
+    height;
+    area;
+    color;
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+        this.area = height * width;
+        this.perimeter=2*(height+width);
     }
-   
-    calcArea(){
-        return this.height*this.width;
+
+    calcArea() {
+        return this.height * this.width;
     }
 }
 
-let myRectangle = new rectangle(10,10);
-console.log(myRectangle.calcArea());
+let myRectangle_1 = new rectangle(10, 5);
+console.log(myRectangle_1.area);
+console.log(myRectangle_1.perimeter);
+
+let myRectangle_2 = new rectangle(10, 3);
+console.log(myRectangle_2.calcArea());
+
+myRectangle_2.color="yellow";
+
+console.log(myRectangle_2.color);
