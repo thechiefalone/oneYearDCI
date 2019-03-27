2f2fd8978f95375ee84257780fb2ab0ec8dddeee
thechiefalone <alperenozkan@gmail.com>
2019-03-27 16:21:56 +0100

    pagination

--- FBW2Hamburg/2019_MARCH/pagination.js
+++ FBW2Hamburg/2019_MARCH/pagination.js
@@ -1,12 +1,12 @@
 var apiKey = "12000491-41fc68d8c365df909e022ceb6";
 var perPageValue;
 var img;
-
+var counter = 2;
 
 function buttonClick() {
     var keyword = document.getElementById("search").value;
     var category = document.getElementById("category").value;
-    var perPageValue = document.getElementById("per-page").value;
+    perPageValue = document.getElementById("per-page").value;
 
     fetch("https://pixabay.com/api/?key=" + apiKey + "&q=" + keyword + "&image_type=photo&category=" + category + "&pretty=true&per_page=" + perPageValue).
     then(function (response) {
@@ -14,40 +14,45 @@ function buttonClick() {
             return;
         }
         response.json().then(function (data) {
-            // getImage(data);
-            var div = document.getElementById("img-container");
-            div.innerHTML = "";
-            for (let i = 0; i < perPageValue; i++) {
-                let img = document.createElement("img");
-                img.src = data.hits[i].largeImageURL;
-                div.appendChild(img);
-            }
+            getImage(data);
         })
     });
 }
 
-// function next() {
-//     var keyword = document.getElementById("search").value;
-//     var category = document.getElementById("category").value;
-//     var perPageValue = document.getElementById("per-page").value;
+function next() {
+    var keyword = document.getElementById("search").value;
+    var category = document.getElementById("category").value;
+    perPageValue = document.getElementById("per-page").value;
+    fetch("https://pixabay.com/api/?key=" + apiKey + "&q=" + keyword + "&image_type=photo&category=" + category + "&pretty=true&per_page=" + perPageValue + "&page=" + counter).
+    then(function (response) {
+        if (response.status !== 200) {
+            return;
+        }
+        response.json().then(function (data) {
+            getImage(data)
+            console.log(data);
+        })
+    });
+    counter++;
+}
 
-//     fetch("https://pixabay.com/api/?key=" + apiKey + "&q=" + keyword + "&image_type=photo&category=" + category + "&pretty=true&per_page=" + perPageValue + "page=2").
-//     then(function (response) {
-//         if (response.status !== 200) {
-//             return;
-//         }
-//         response.json().then(function (data) {
-//             getImage(data)
-//         })
-//     });
-// }
+function getImage(anyData) {
+    var div = document.getElementById("img-container");
+    div.innerHTML = "";
+    for (let i = 0; i < perPageValue; i++) {
+        img = document.createElement("img");
+        div.appendChild(img);
+        img.src = anyData.hits[i].largeImageURL;
+        img.setAttribute("class","images");
+        img.setAttribute("onclick","imgClickOpen(this)");
+    }
+}
 
-// function getImage(anyData) {
-//     var div = document.getElementById("img-container");
-//     div.innerHTML = "";
-//     for (let i = 0; i < perPageValue; i++) {
-//         let img = document.createElement("img");
-//         img.src = anyData.hits[i].largeImageURL;
-//         div.appendChild(img);
-//     }
+function imgClickOpen(img) {
+    img.setAttribute("class","zoom-out");
+}
+
+// function imgClickClose(img){
+//     img.setAttribute("class","zoom-out");
+
 // }
