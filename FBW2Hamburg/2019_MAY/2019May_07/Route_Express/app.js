120ba928bc90ea6140e86f68033857b0e2905dc6
thechiefalone <alperenozkan@gmail.com>
2019-05-07 14:40:35 +0200

    ok

--- FBW2Hamburg/2019_MAY/2019May_07/Route_Express/app.js
+++ FBW2Hamburg/2019_MAY/2019May_07/Route_Express/app.js
@@ -4,14 +4,46 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set('views', path.join(__dirname,'./src/views'));
+app.set('views', path.join(__dirname, './src/views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname,'/public')));
-app.get('/',(req,res)=>{
+//Navigation Data Global --Start--
+let indexData = {
+    title: "Books Store",
+    nav: [{
+        title: 'Authors',
+        link: '/authors'
+    }, {
+        title: 'Books',
+        link: '/books'
+    }, {
+        title: 'Publishers',
+        link: '/publishers'
+    }]
+};
+//Navigation Data Global --End--
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.get('/', (req, res) => {
     // res.sendFile(path.join(__dirname,'/src/views/index.html'));
-res.render('index');
+    indexData.title="Books Store"
+    res.render('index', indexData);
+
 });
 
+app.get('/authors', (req, res) => {
+    res.send("Hello Authors");
+});
 
-app.listen(port, ()=>{console.log(`server strated at port number ${port}`);});
+app.get('/books', (req, res) => {
+    indexData.title="Books"
+    res.render('books', indexData);
+});
+
+app.get('/publishers', (req, res) => {
+    res.send("Hello Publishers");
+});
+
+app.listen(port, () => {
+    console.log(`server strated at port number ${port}`);
+});
